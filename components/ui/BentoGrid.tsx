14cf3eedb18f0562c490fda99db729190ce0dffd
Ankit kumar <125955449+Ankitkumar1100@users.noreply.github.com>
2024-07-04 14:22:31 +0530

    done

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -78,8 +78,6 @@ export const BentoGridItem = ({
             </BackgroundGradientAnimation>
 
         )}
-
-
         <div className={cn(
             titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10' 
         )}>
